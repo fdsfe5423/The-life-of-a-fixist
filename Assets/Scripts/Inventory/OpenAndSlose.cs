3033bee1307659d3e08f9fcd9cf1e65ce10d2d01
itcube22 <104897682+itcube22@users.noreply.github.com>
2024-11-21 10:47:07 +0700

    ыавфыа

---
+++ Assets/Scripts/Inventory/OpenAndSlose.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenAndSlose : MonoBehaviour
+{
+    public bool isOpen;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
