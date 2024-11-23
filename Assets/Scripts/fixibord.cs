384bb67b7a8111cf9281662acb0b5e69c0067a85
itcube22 <104897682+itcube22@users.noreply.github.com>
2024-11-23 12:03:53 +0700

    dgdfgf

--- Assets/Scripts/fixibord.cs
+++ Assets/Scripts/fixibord.cs
@@ -3,22 +3,51 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using Unity.XR.CoreUtils;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
+using System.Runtime.InteropServices;
 
 public class fixibord : MonoBehaviour
 {
-    [SerializeField] GameObject fixbord;
+    [SerializeField] GameObject fixbordPoint;
     [SerializeField] GameObject fixbordPlayer;
-    [SerializeField] DynamicMoveProvider player;
+    [SerializeField] GameObject player;
+    public bool iFixiboard;
+
+    private void Update()
+    {
+        fixbordPoint = GameObject.Find("PointFixiboard");
+        player = GameObject.Find("Move");
+        var continiousMoveProvider = player.GetComponent<ContinuousMoveProviderBase>();
+        if(iFixiboard)
+        {
+            fixbordPlayer.SetActive(false);
+            fixbordPoint.SetActive(true);
+            continiousMoveProvider.enableFly = true;
+            continiousMoveProvider.moveSpeed = 1;
+        }
+        else
+        {
+            fixbordPlayer.SetActive(true);
+            fixbordPoint.SetActive(false);
+            continiousMoveProvider.enableFly = true;
+            continiousMoveProvider.moveSpeed = 10;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.GetComponent<CharacterController>() != null)
         {
-            fixbordPlayer.SetActive(!false);
-            fixbord.SetActive(false);
-            player.enableFly = true;
-            player.useGravity = false;
+            iFixiboard = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent<CharacterController>() != null)
+        {
+            iFixiboard = false;
         }
     }
 }
