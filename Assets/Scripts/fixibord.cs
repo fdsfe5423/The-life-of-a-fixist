c7d84fcfc2a7850bee6226e1bacb1717f686ae83
itcube22 <104897682+itcube22@users.noreply.github.com>
2024-11-28 12:11:47 +0700

    hhf

--- Assets/Scripts/fixibord.cs
+++ Assets/Scripts/fixibord.cs
@@ -5,49 +5,50 @@ using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.InputSystem;
-using System.Runtime.InteropServices;
 
 public class fixibord : MonoBehaviour
 {
-    [SerializeField] GameObject fixbordPoint;
-    [SerializeField] GameObject fixbordPlayer;
-    [SerializeField] GameObject player;
     public bool iFixiboard;
+    public bool iFIXI;
+    public GameObject point;
+    public GameObject player;
+    public ContinuousMoveProviderBase move;
+    public GameObject camera;
+    public InputActionProperty acrionDropFIXI;
 
     private void Update()
     {
-        fixbordPoint = GameObject.Find("PointFixiboard");
+        camera = GameObject.Find("Main Camera");
         player = GameObject.Find("Move");
-        var continiousMoveProvider = player.GetComponent<ContinuousMoveProviderBase>();
+        move = player.GetComponent<ContinuousMoveProviderBase>();
+        point = GameObject.Find("PointFixi");
+        float Drop = acrionDropFIXI.action.ReadValue<float>();
+            if(Drop > 0.8)
+            {
+                iFixiboard = false;
+                iFIXI = false;
+                Invoke("FIXI", 1);
+            }
         if(iFixiboard)
         {
-            fixbordPlayer.SetActive(false);
-            fixbordPoint.SetActive(true);
-            continiousMoveProvider.enableFly = true;
-            continiousMoveProvider.moveSpeed = 1;
-        }
-        else
-        {
-            fixbordPlayer.SetActive(true);
-            fixbordPoint.SetActive(false);
-            continiousMoveProvider.enableFly = true;
-            continiousMoveProvider.moveSpeed = 10;
+            gameObject.transform.position = point.transform.position;
+            Quaternion gameobjRot = gameObject.transform.localRotation;
+            gameObject.transform.localEulerAngles = new Vector3(0, camera.transform.localEulerAngles.y, 0);
+            move.enableFly = true;
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.GetComponent<CharacterController>() != null)
+        if(other.gameObject.GetComponent<CharacterController>() != null && !iFixiboard && iFIXI)
         {
+            
             iFixiboard = true;
+            move.moveSpeed *= 3;
         }
     }
-
-    private void OnTriggerExit(Collider other)
+    public void FIXI()
     {
-        if (other.gameObject.GetComponent<CharacterController>() != null)
-        {
-            iFixiboard = false;
-        }
+        iFIXI = true;
     }
 }
