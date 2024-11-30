ebbf1836a0ee2991c2485d41fc860dfd57969599
itcube22 <104897682+itcube22@users.noreply.github.com>
2024-11-30 10:47:16 +0700

    fff

--- Assets/Scripts/Inventory/OpenAndSlose.cs
+++ Assets/Scripts/Inventory/OpenAndSlose.cs
@@ -1,13 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
+using JetBrains.Annotations;
 
 public class OpenAndSlose : MonoBehaviour
 {
+    public GameObject UI;
     public bool isOpen;
+    public InputActionProperty action;
+
+    private void Update()
+    {
+        float iNT = action.action.ReadValue<float>();
+        if(isOpen)
+        {
+            UI.SetActive(true);
+        }
+        else
+        {
+            UI.SetActive(false);
+        }
+        if(iNT > 0.7)
+        {
+            if (isOpen)
+            {
+                UI.SetActive(true);
+                isOpen = false;
+            }
+            else
+            {
+                UI.SetActive(false);
+                isOpen = true;
+            }
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        
+      if(other.gameObject.tag == "Hand")
+        {
+            isOpen = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Hand")
+        {
+            isOpen = true;
+        }
     }
 }
