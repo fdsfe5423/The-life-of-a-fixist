df3cfc3e3ba407d0d96317c8bd692d2b3ea04f51
fdsfe5423 <136370210+fdsfe5423@users.noreply.github.com>
2024-12-22 20:49:21 +0700

    hhh

--- Assets/Scripts/Tool.cs
+++ Assets/Scripts/Tool.cs
@@ -10,33 +10,11 @@ public class Tool : MonoBehaviour
     public bool iNeedActivet;
     public GameObject triggerObj;
 
-    private void OnTriggerStay(Collider other)
-    {
-        if(other.gameObject.GetComponent<Repair>().name == nameTool)
-        {
-           if(iNeedActivet)
-            {
-                triggerObj = other.gameObject;
-            }
-        }
-    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<Repair>().name == nameTool)
         {
-            if (!iNeedActivet)
-            {
-                other.gameObject.GetComponent<Repair>().REPAir();
-            }
+           other.gameObject.GetComponent<Repair>().REPAir();
         }
-    }
-    public IEnumerator Active()
-    {
-        if(iActivate)
-        {
-            gameObject.GetComponent<Repair>().REPAirActivate();
-            audio.Play();
-        }
-        yield return new WaitForSeconds(0.3f);
     }
 }
